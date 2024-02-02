7001f1ec0dade445597eb6a4be2a22c0a900c0ae
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-02-02 19:28:48 +0800

    create starting Springboot project

---
+++ src/main/java/gov/coateam1/TravelManagementApplication.java
@@ -0,0 +1,13 @@
+package gov.coateam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/coateam1/TravelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.coateam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
