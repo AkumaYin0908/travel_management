9bfc09f1ee53e458cd0f3bd9aec1e7cd912303d0
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-01 16:45:38 +0800

    change data type of position field from String to PositionDTO

--- src/main/java/gov/coateam1/payload/employee/EmployeeDTO.java
+++ src/main/java/gov/coateam1/payload/employee/EmployeeDTO.java
@@ -3,6 +3,7 @@ package gov.coateam1.payload.employee;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import gov.coateam1.model.TravelOrder;
 import gov.coateam1.model.TripTicket;
+import gov.coateam1.payload.PositionDTO;
 import gov.coateam1.payload.TravelOrderDTO;
 import gov.coateam1.payload.TripTicketDTO;
 import lombok.AllArgsConstructor;
@@ -20,7 +21,7 @@ public class EmployeeDTO{
 
     private Long id;
     private String name;
-    private String position;
+    private PositionDTO position;
 
     @JsonIgnore
     private List<TravelOrderDTO> travelOrders;
@@ -28,7 +29,7 @@ public class EmployeeDTO{
     private List<TripTicketDTO> tripTickets;
 
 
-    public EmployeeDTO(Long id, String name, String position) {
+    public EmployeeDTO(Long id, String name, PositionDTO position) {
         this.id = id;
         this.name = name;
         this.position = position;
