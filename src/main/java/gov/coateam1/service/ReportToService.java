fbccf7c3d71fb83ff62708691af56d81812486e0
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-02 17:01:40 +0800

    change return type of ReportToService methods from model to DTO

--- src/main/java/gov/coateam1/service/ReportToService.java
+++ src/main/java/gov/coateam1/service/ReportToService.java
@@ -2,18 +2,20 @@ package gov.coateam1.service;
 
 import gov.coateam1.model.ReportTo;
 import gov.coateam1.model.TravelOrder;
+import gov.coateam1.payload.ReportToDTO;
+import gov.coateam1.payload.SignatoryDTO;
 
 import java.util.List;
 
 public interface ReportToService {
 
 
-    List<ReportTo> findAll();
-    ReportTo findByName(String name);
+    List<ReportToDTO> findAll();
+    ReportToDTO findByName(String name);
 
-    ReportTo add(ReportTo reportTo);
+    ReportToDTO add(ReportToDTO reportToDTO);
 
-    ReportTo update(ReportTo reportTo);
+    ReportToDTO update(ReportToDTO reportToDTO, Long id);
 
     void delete(Long id);
 
