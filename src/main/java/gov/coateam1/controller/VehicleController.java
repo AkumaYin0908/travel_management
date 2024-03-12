8c67b3b2dcf36d2e5f63d4ac2b2f09831df3f7b4
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-12 21:23:26 +0800

    create vehicle controller

---
+++ src/main/java/gov/coateam1/controller/VehicleController.java
@@ -0,0 +1,16 @@
+package gov.coateam1.controller;
+
+import gov.coateam1.service.VehicleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vehicles")
+@RequiredArgsConstructor
+public class VehicleController {
+
+
+
+}
