95a324efbb162dfc465957155b42b434367cd486
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-15 16:58:10 +0800

    create AppConstant

---
+++ src/main/java/gov/coateam1/constants/AppConstant.java
@@ -0,0 +1,9 @@
+package gov.coateam1.constants;
+
+public interface AppConstant {
+
+    String BARANGAY_JSON = "ph-json/barangay.json";
+    String MUNICIPALITY_JSON = "ph-json/city.json";
+    String PROVINCE_JSON = "ph-json/province.json";
+    String REGION_JSON = "ph-json/region.json";
+}
