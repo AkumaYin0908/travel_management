fbba0bef981881ce5d592071ca8037ddd5b9f468
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-15 14:14:01 +0800

    create PlaceQueryConstant interface

---
+++ src/main/java/gov/coateam1/constants/PlaceQueryConstant.java
@@ -0,0 +1,21 @@
+package gov.coateam1.constants;
+
+public interface PlaceQueryConstant {
+
+    String FINDBY_BUILDINGNAME = "select" +
+            " building_name," +
+            " b.barangay_code," +
+            " b.barangay_name," +
+            " m.municipality_code," +
+            " m.municipality_name," +
+            " p.province_code," +
+            " p.province_name," +
+            " r.region_code," +
+            " r.region_name" +
+            " from place inner join barangay as b on place.barangay = b.barangay_code" +
+            " inner join municipality as m on place.municipality = m.municipality_code" +
+            " inner join province as p on place.province = p.province_code" +
+            " inner join region as r on place.region = r.region_code" +
+            " where building_name = ?1";
+
+}
