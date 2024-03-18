9a3659e153eb52171b361524cc3f6746699602f3
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-19 03:35:55 +0800

    remove commented method

--- src/main/java/gov/coateam1/service/TravelOrderService.java
+++ src/main/java/gov/coateam1/service/TravelOrderService.java
@@ -11,28 +11,11 @@ public interface TravelOrderService {
 
     List<TravelOrderDTO> findAll();
 
-  //  TravelOrderDTO findByDateDepartureAndDateReturn(LocalDate dateDeparture, LocalDate dateReturn);
-
     TravelOrderDTO findById(Long id);
 
     TravelOrderDTO add(Long employeeId, TravelOrderDTO travelOrderDTO) throws Exception;
 
     TravelOrderDTO update(TravelOrderDTO travelOrderDTO, Long id) throws Exception;
-
-//    List<TravelOrderDTO> findTravelOrderAndReportTosById(Long id);
-//
-//    List<TravelOrderDTO> findTravelOrderAndPlacesById(Long id);
-//
-//    List<TravelOrderDTO> findByBuildingName(String buildingName);
-//
-//    List<TravelOrderDTO> findByBarangay(String barangay);
-//
-//    List<TravelOrderDTO> findByMunicipality(String municipality);
-//
-//    List<TravelOrderDTO> findByProvince(String province);
-//
-//
-//    LocalDate findByNameOrderByDateReturnDESC(String name,LocalDate dateReturn);
 
     void delete(Long id);
 
