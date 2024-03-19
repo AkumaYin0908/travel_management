5e6d01396e100b1ba108800e676f96d89fc5cd9f
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-19 21:04:50 +0800

    clear the method implementation of methods inherited to TripTicketService

--- src/main/java/gov/coateam1/service/impl/TripTicketServiceImpl.java
+++ src/main/java/gov/coateam1/service/impl/TripTicketServiceImpl.java
@@ -30,60 +30,13 @@ import java.util.List;
 public class TripTicketServiceImpl implements TripTicketService {
 
     private final TripTicketRepository tripTicketRepository;
-    private final FuelBalanceService fuelBalanceService;
-    private final DistanceService distanceService;
-    private final PlaceService placeService;
 
     private final ModelMapper modelMapper;
-
-    private static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
-
-    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
-
-
-    private TripTicketDTO convertToDTO(TripTicket tripTicket){
-        Converter<LocalDate, String> toDateString =
-                context -> context.getSource() == null ? null : dateFormatter.format(context.getSource());
-
-        Converter<LocalTime,String> toTimeString =
-                context -> context.getSource() == null ? null : timeFormatter.format(context.getSource());
-
-        modelMapper.typeMap(TripTicket.class, TripTicketDTO.class)
-                .addMappings(mapping -> mapping.using(toDateString).map(TripTicket::getDateDeparture,TripTicketDTO::setDateDeparture))
-                .addMappings(mapping -> mapping.using(toDateString).map(TripTicket::getDateReturn,TripTicketDTO::setDateReturn))
-                .addMappings(mapping -> mapping.using(toTimeString).map(TripTicket::getTimeOfficeDeparture,TripTicketDTO::setTimeOfficeDeparture))
-                .addMappings(mapping -> mapping.using(toTimeString).map(TripTicket::getTimePlaceArrival,TripTicketDTO::setTimePlaceArrival))
-                .addMappings(mapping -> mapping.using(toTimeString).map(TripTicket::getTimePlaceDeparture,TripTicketDTO::setTimePlaceDeparture))
-                .addMappings(mapping -> mapping.using(toTimeString).map(TripTicket::getTimeOfficeArrival,TripTicketDTO::setTimeOfficeArrival));
-
-    return modelMapper.map(tripTicket,TripTicketDTO.class);
-    }
-
-
-    private TripTicket convertToModel(TripTicketDTO tripTicketDTO){
-        Converter<String,LocalDate> toLocalDate=
-        context -> context.getSource() == null ? null : LocalDate.parse(context.getSource(),dateFormatter);
-
-        Converter<String, LocalTime> toLocalTime =
-        context -> context.getSource() == null ? null : LocalTime.parse(context.getSource(),timeFormatter);
-
-        modelMapper.typeMap(TripTicketDTO.class, TripTicket.class)
-                .addMappings(mapping -> mapping.using(toLocalDate).map(TripTicketDTO::getDateDeparture, TripTicket::setDateDeparture))
-                .addMappings(mapping -> mapping.using(toLocalDate).map(TripTicketDTO::getDateReturn, TripTicket::setDateReturn))
-                .addMappings(mapping -> mapping.using(toLocalTime).map(TripTicketDTO::getTimeOfficeDeparture, TripTicket::setTimeOfficeDeparture))
-                .addMappings(mapping -> mapping.using(toLocalTime).map(TripTicketDTO::getTimePlaceArrival, TripTicket::setTimePlaceArrival))
-                .addMappings(mapping -> mapping.using(toLocalTime).map(TripTicketDTO::getTimePlaceDeparture, TripTicket::setTimePlaceDeparture))
-                .addMappings(mapping -> mapping.using(toLocalTime).map(TripTicketDTO::getTimeOfficeArrival, TripTicket::setTimeOfficeArrival));
-
-        return modelMapper.map(tripTicketDTO,TripTicket.class);
-    }
-
 
 
     @Override
     public List<TripTicketDTO> findAll() {
-        return tripTicketRepository.findAll().stream().map(this::convertToDTO).toList();
-
+        return null;
     }
 
     @Override
@@ -92,52 +45,13 @@ public class TripTicketServiceImpl implements TripTicketService {
     }
 
     @Override
-    @Transactional
     public TripTicketDTO add(TripTicketDTO tripTicketDTO) {
-
-        TripTicket tripTicket = this.convertToModel(tripTicketDTO);
-        FuelBalance fuelBalance = fuelBalanceService.findFuelBalance();
-        tripTicket.setFuelBalance(fuelBalance.getFuel());
-
-        Distance distance = distanceService.findDistance();
-        tripTicket.setStartDistance(distance.getDistance());
-
-        TripTicket dbTripTicket = tripTicketRepository.save(tripTicket) ;
-        tripTicketDTO.setId(dbTripTicket.getId());
-        return tripTicketDTO;
+        return null;
     }
 
     @Override
-    @Transactional
     public TripTicketDTO update(TripTicketDTO tripTicketDTO, Long id) {
-        TripTicket tripTicket = tripTicketRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("TripTicket","id",id));
-        tripTicket.setDriver(modelMapper.map(tripTicketDTO.getEmployee(), Driver.class));
-        tripTicket.setDateDeparture(DateTimeConverter.convertToLocalDate(tripTicketDTO.getDateDeparture()));
-        tripTicket.setDateReturn(DateTimeConverter.convertToLocalDate(tripTicketDTO.getDateReturn()));
-        tripTicket.setPlaces(tripTicketDTO.getPlaces().stream().map(p->modelMapper.map(p, Place.class)).toList());
-        tripTicket.setPassengers(tripTicketDTO.getPassengers().stream().map(p->modelMapper.map(p, Passenger.class)).toList());
-        tripTicket.setTimeOfficeDeparture(DateTimeConverter.convertToLocalTime(tripTicketDTO.getTimeOfficeDeparture()));
-        tripTicket.setTimePlaceArrival(DateTimeConverter.convertToLocalTime(tripTicketDTO.getTimePlaceArrival()));
-        tripTicket.setTimeOfficeDeparture(DateTimeConverter.convertToLocalTime(tripTicketDTO.getTimePlaceArrival()));
-        tripTicket.setFuelBalance(tripTicketDTO.getFuelBalance());
-        tripTicket.setIssuedFuel(tripTicketDTO.getIssuedFuel());
-        tripTicket.setPurchasedFuel(tripTicketDTO.getPurchasedFuel());
-        tripTicket.setConsumedFuel(tripTicketDTO.getConsumedFuel());
-        tripTicket.setRemainingFuel(tripTicketDTO.getRemainingFuel());
-        tripTicket.setGearOil(tripTicketDTO.getGearOil());
-        tripTicket.setLubricantOil(tripTicketDTO.getLubricantOil());
-        tripTicket.setStartDistance(tripTicketDTO.getStartDistance());
-        tripTicket.setEndDistance(tripTicketDTO.getEndDistance());
-        tripTicket.setDistanceTravelled(tripTicketDTO.getDistanceTravelled());
-        tripTicket.setRemarks(tripTicketDTO.getRemarks());
-        tripTicket.setPurpose(modelMapper.map(tripTicketDTO.getPurpose(), Purpose.class));
-        tripTicket.setVehicle(modelMapper.map(tripTicketDTO.getVehicle(), Vehicle.class));
-        tripTicketRepository.save(tripTicket);
-        fuelBalanceService.update(tripTicketDTO.getRemainingFuel());
-        distanceService.update(tripTicketDTO.getEndDistance());
-        tripTicketDTO.setId(tripTicket.getId());
-
-        return tripTicketDTO;
+        return null;
     }
 
     @Override
