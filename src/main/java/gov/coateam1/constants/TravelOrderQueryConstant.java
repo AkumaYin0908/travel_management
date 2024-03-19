ad1ca356233751e8d8a2b3666bdce2084fea098f
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-19 19:47:28 +0800

    removed unnecessary character in sql statement

--- src/main/java/gov/coateam1/constants/TravelOrderQueryConstant.java
+++ src/main/java/gov/coateam1/constants/TravelOrderQueryConstant.java
@@ -2,5 +2,5 @@ package gov.coateam1.constants;
 
 public interface TravelOrderQueryConstant {
 
-    String FIND_BY_DATE_ISSUED = "select * from travel_order where date_issued = ?1";
+    String FIND_BY_DATE_ISSUED = "select * from travel_order where date_issued = ?";
 }
