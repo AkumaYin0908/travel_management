df19149c8ac29be6a61588248b0d19d2d990e4b9
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-19 21:00:24 +0800

    remove unused import statements

--- src/main/java/gov/coateam1/payload/VehicleDTO.java
+++ src/main/java/gov/coateam1/payload/VehicleDTO.java
@@ -1,11 +1,6 @@
 package gov.coateam1.payload;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import gov.coateam1.model.TripTicket;
 import lombok.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
--- src/main/java/gov/coateam1/payload/employee/EmployeeDTO.java
+++ src/main/java/gov/coateam1/payload/employee/EmployeeDTO.java
@@ -1,15 +1,8 @@
 package gov.coateam1.payload.employee;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import gov.coateam1.model.TravelOrder;
-import gov.coateam1.model.TripTicket;
 import gov.coateam1.payload.PositionDTO;
-import gov.coateam1.payload.TravelOrderDTO;
-import gov.coateam1.payload.TripTicketDTO;
 import lombok.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Getter
