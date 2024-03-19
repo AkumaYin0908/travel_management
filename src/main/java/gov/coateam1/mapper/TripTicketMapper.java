c8fe3b0eae7ac0eee0dd53cc7e01e53e3d6ad485
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-20 05:40:54 +0800

    create convertToModel and convertToDTO for TripTime and finish implementation of maptoModel

--- src/main/java/gov/coateam1/mapper/TripTicketMapper.java
+++ src/main/java/gov/coateam1/mapper/TripTicketMapper.java
@@ -2,16 +2,30 @@ package gov.coateam1.mapper;
 
 
 import gov.coateam1.functionalinterface.CheckedFunction;
+import gov.coateam1.model.Purpose;
+import gov.coateam1.model.Vehicle;
+import gov.coateam1.model.trip_ticket.TripDistance;
+import gov.coateam1.model.trip_ticket.TripFuel;
 import gov.coateam1.model.trip_ticket.TripTicket;
 import gov.coateam1.model.employee.Driver;
 import gov.coateam1.model.employee.Passenger;
 import gov.coateam1.model.place.Place;
-import gov.coateam1.payload.TripTicketDTO;
+import gov.coateam1.model.trip_ticket.TripTime;
+import gov.coateam1.payload.trip_ticket.TripFuelDTO;
+import gov.coateam1.payload.trip_ticket.TripTicketDTO;
+import gov.coateam1.payload.trip_ticket.TripTimeDTO;
+import gov.coateam1.util.DateTimeConverter;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -21,18 +35,60 @@ public class TripTicketMapper {
     private final VehicleMapper vehicleMapper;
     private final EmployeeMapper employeeMapper;
     private final PurposeMapper purposeMapper;
+    private final ModelMapper modelMapper;
 
 
+    private TripTimeDTO convertToDTO(TripTime tripTime){
+
+        Converter<LocalTime,String> toTimeString =
+                context -> context.getSource() == null ? null : DateTimeConverter.convertToString(context.getSource());
+
+        modelMapper.typeMap(TripTime.class, TripTimeDTO.class)
+                .addMappings(mapping -> mapping.using(toTimeString).map(TripTime::getTimeOfficeDeparture,TripTimeDTO::setTimeOfficeDeparture))
+                .addMappings(mapping -> mapping.using(toTimeString).map(TripTime::getTimePlaceArrival,TripTimeDTO::setTimePlaceArrival))
+                .addMappings(mapping -> mapping.using(toTimeString).map(TripTime::getTimePlaceDeparture,TripTimeDTO::setTimePlaceDeparture))
+                .addMappings(mapping -> mapping.using(toTimeString).map(TripTime::getTimeOfficeArrival,TripTimeDTO::setTimeOfficeArrival));
+
+        return modelMapper.map(tripTime,TripTimeDTO.class);
+    }
+
+
+    private TripTime convertToModel(TripTimeDTO tripTimeDTO){
+
+
+        Converter<String, LocalTime> toLocalTime =
+                context -> context.getSource() == null ? null : DateTimeConverter.convertToLocalTime(context.getSource());
+
+        Converter<LocalTime,String> toTimeString =
+                context -> context.getSource() == null ? null : DateTimeConverter.convertToString(context.getSource());
+
+        modelMapper.typeMap(TripTimeDTO.class,TripTime.class)
+                .addMappings(mapping -> mapping.using(toTimeString).map(TripTimeDTO::getTimeOfficeDeparture,TripTime::setTimeOfficeDeparture))
+                .addMappings(mapping -> mapping.using(toTimeString).map(TripTimeDTO::getTimePlaceArrival,TripTime::setTimePlaceArrival))
+                .addMappings(mapping -> mapping.using(toTimeString).map(TripTimeDTO::getTimePlaceDeparture,TripTime::setTimePlaceDeparture))
+                .addMappings(mapping -> mapping.using(toTimeString).map(TripTimeDTO::getTimeOfficeArrival,TripTime::setTimeOfficeArrival));
+
+        return modelMapper.map(tripTimeDTO,TripTime.class);
+    }
 
     public TripTicket mapToModel(TripTicketDTO tripTicketDTO) throws Exception {
-        Driver driver = employeeMapper.maptoModel(tripTicketDTO.getEmployee(), Driver.class);
-        List<Place> places = tripTicketDTO.getPlaces().stream().map(placeMapper::mapToModel).toList();
-        List<Passenger> passengers = tripTicketDTO.getPassengers().stream().map(throwingFunction(emp->employeeMapper.maptoModel(emp,Passenger.class))).toList();
+        LocalDate dateDeparture = DateTimeConverter.convertToLocalDate(tripTicketDTO.getDateDeparture());
+        LocalDate dateReturn = DateTimeConverter.convertToLocalDate(tripTicketDTO.getDateReturn());
+        Driver driver = modelMapper.map(tripTicketDTO.getEmployee(),Driver.class);
+        Set<Place> places = tripTicketDTO.getPlaces().stream().map(placeMapper::mapToModel).collect(Collectors.toSet());
+        Set<Passenger> passengers = tripTicketDTO.getPassengers().stream().map(throwingFunction(emp->employeeMapper.maptoModel(emp,Passenger.class))).collect(Collectors.toSet());
+        TripTime tripTime = this.convertToModel(tripTicketDTO.getTripTime());
+        TripFuel tripFuel = modelMapper.map(tripTicketDTO.getTripFuel(),TripFuel.class);
+        TripDistance tripDistance = modelMapper.map(tripTicketDTO.getTripDistance(),TripDistance.class);
+        Purpose purpose = modelMapper.map(tripTicketDTO.getPurpose(),Purpose.class);
+        Vehicle vehicle = modelMapper.map(tripTicketDTO.getVehicle(), Vehicle.class);
 
 
 
-
-        return null;
+        return new TripTicket(
+                tripTicketDTO.getId(), driver, dateDeparture, dateReturn, places, passengers, tripTime, tripFuel,
+                tripTicketDTO.getGearOil(), tripTicketDTO.getLubricantOil(), tripDistance, tripTicketDTO.getRemarks(),
+                purpose,vehicle);
     }
 
 
