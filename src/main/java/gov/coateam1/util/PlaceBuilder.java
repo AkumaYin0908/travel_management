a7817bb68b23e3648f6a6a66542ee48ad2c6d33c
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-21 03:25:05 +0800

    create PlaceBuilder

---
+++ src/main/java/gov/coateam1/util/PlaceBuilder.java
@@ -0,0 +1,45 @@
+package gov.coateam1.util;
+
+import gov.coateam1.constants.AppConstant;
+import gov.coateam1.model.place.*;
+import gov.coateam1.payload.place.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PlaceBuilder {
+
+    private final JSONDataLoader jsonDataLoader;
+
+    public Place constructPlace(PlaceDTO placeDTO) throws Exception {
+        Place place = new Place();
+        place.setBuildingName(placeDTO.getBuildingName());
+
+        if (placeDTO.getBarangay() == null) {
+            place.setBarangay(null);
+        } else {
+            BarangayDTO barangayDTO = jsonDataLoader.getFromCode(placeDTO.getBarangay().getCode(), AppConstant.BARANGAY_JSON, BarangayDTO.class);
+            Barangay barangay = new Barangay(barangayDTO.getCode(), barangayDTO.getName());
+            barangay.addPlace(place);
+            place.setBarangay(barangay);
+        }
+        MunicipalityDTO municipalityDTO = jsonDataLoader.getFromCode(placeDTO.getMunicipality().getCode(), AppConstant.MUNICIPALITY_JSON, MunicipalityDTO.class);
+        Municipality municipality = new Municipality(municipalityDTO.getCode(), municipalityDTO.getName());
+        municipality.addPlace(place);
+        place.setMunicipality(municipality);
+
+        ProvinceDTO provinceDTO = jsonDataLoader.getFromCode(placeDTO.getProvince().getCode(), AppConstant.PROVINCE_JSON, ProvinceDTO.class);
+        Province province = new Province(provinceDTO.getCode(), provinceDTO.getName());
+        province.addPlace(place);
+        place.setProvince(province);
+
+        RegionDTO regionDTO = jsonDataLoader.getFromCode(placeDTO.getRegionDTO().getCode(), AppConstant.REGION_JSON, RegionDTO.class);
+        Region region = new Region(regionDTO.getCode(), regionDTO.getName());
+        region.addPlace(place);
+        place.setRegion(region);
+
+    return place;
+    }
+}
+
