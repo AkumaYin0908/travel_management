dbc788d72fa8233ec258e925dba1a04d2edeaf5b
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-23 21:14:51 +0800

    override equals and hashcode method and create public method id() for testing purposes

--- src/main/java/gov/coateam1/payload/VehicleDTO.java
+++ src/main/java/gov/coateam1/payload/VehicleDTO.java
@@ -2,6 +2,8 @@ package gov.coateam1.payload;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,10 +16,28 @@ public class VehicleDTO {
     private String type;
     private String plateNo;
 
-
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VehicleDTO that = (VehicleDTO) o;
+        return Objects.equals(id, that.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+        @Override
     public String toString() {
         return String.format("%s %s/%s/%s",
                 brand,model,type,plateNo);
+    }
+
+    //for testing purposes
+    public VehicleDTO id(Long id){
+        this.id=id;
+        return this;
     }
 }
