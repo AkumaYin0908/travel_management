5f9fe04fed0ff64ca4dec5c665558ebb19e64f77
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-25 04:38:22 +0800

    remove custom mappers to map Barangay,Municipality,Province and Region to DTO and vice versa;use ModelMapper and JSONDataLoader instead

--- src/main/java/gov/coateam1/mapper/PlaceMapper.java
+++ src/main/java/gov/coateam1/mapper/PlaceMapper.java
@@ -1,49 +1,19 @@
 package gov.coateam1.mapper;
 
+import gov.coateam1.constants.AppConstant;
 import gov.coateam1.model.place.*;
 import gov.coateam1.payload.place.*;
+import gov.coateam1.util.JSONDataLoader;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class PlaceMapper {
 
-    public BarangayDTO toBarangayDTO(Barangay barangay){
-       return barangay == null ? null : new BarangayDTO(barangay.getBrgyCode(),barangay.getBrgyName());
-    }
-
-    public Barangay toBarangay(BarangayDTO barangayDTO){
-
-        return barangayDTO==null ? null : new Barangay(barangayDTO.getCode(),barangayDTO.getName());
-    }
-
-    public MunicipalityDTO toMunicipalityDTO(Municipality municipality){
-        return municipality == null ? null : new MunicipalityDTO(municipality.getMunicipalityCode(),municipality.getMunicipalityName());
-    }
-
-    public Municipality toMunicipality(MunicipalityDTO municipalityDTO){
-        return municipalityDTO == null ? null : new Municipality(municipalityDTO.getCode(),municipalityDTO.getName());
-    }
-
-    public ProvinceDTO toProvinceDTO(Province province){
-        return province == null ? null : new ProvinceDTO(province.getProvinceCode(),province.getProvinceName());
-    }
-
-    public Province toProvince(ProvinceDTO provinceDTO){
-        return provinceDTO == null ? null : new Province(provinceDTO.getCode(),provinceDTO.getName());
-    }
-
-    public RegionDTO regionDTO(Region region){
-        return region == null ? null : new RegionDTO(region.getRegionCode(),region.getRegionName());
-    }
-
-    public Region toRegion(RegionDTO regionDTO){
-        return regionDTO == null ? null : new Region(regionDTO.getCode(),regionDTO.getName());
-    }
-
-
-
+    private final ModelMapper modelMapper;
+    private final JSONDataLoader jsonDataLoader;
 
 
     public Place mapToModel(PlaceDTO placeDTO){
@@ -51,47 +21,27 @@ public class PlaceMapper {
             return new Place(placeDTO.getDefaultPlace());
         }
 
-        Barangay barangay=this.toBarangay(placeDTO.getBarangay());
-        Municipality municipality=this.toMunicipality(placeDTO.getMunicipality());
-        Province province=this.toProvince(placeDTO.getProvince());
-        Region region = this.toRegion(placeDTO.getRegion());
+        Barangay barangay= modelMapper.map(placeDTO.getBarangay(),Barangay.class);
+        Municipality municipality=modelMapper.map(placeDTO.getMunicipality(), Municipality.class);
+        Province province=modelMapper.map(placeDTO.getProvince(), Province.class);
+        Region region = modelMapper.map(placeDTO.getRegion(),Region.class);
 
         return new Place(placeDTO.getId(), placeDTO.getBuildingName(),
                 barangay, municipality, province,region);
     }
 
-    public PlaceDTO mapToDTO(Place place){
+    public PlaceDTO mapToDTO(Place place) throws Exception{
         if(place.getDefaultPlace()!=null){
             return new PlaceDTO(place.getDefaultPlace());
         }
 
-        BarangayDTO barangayDTO = this.toBarangayDTO(place.getBarangay());
-        MunicipalityDTO municipalityDTO = this.toMunicipalityDTO(place.getMunicipality());
-        ProvinceDTO provinceDTO = this.toProvinceDTO(place.getProvince());
-        RegionDTO regionDTO = this.regionDTO(place.getRegion());
+        BarangayDTO barangayDTO = jsonDataLoader.getFromCode(place.getBarangay().getBrgyCode(), AppConstant.BARANGAY_JSON, BarangayDTO.class);
+        MunicipalityDTO municipalityDTO = jsonDataLoader.getFromCode(place.getMunicipality().getMunicipalityCode(),AppConstant.MUNICIPALITY_JSON, MunicipalityDTO.class);
+        ProvinceDTO provinceDTO = jsonDataLoader.getFromCode(place.getProvince().getProvinceCode(),AppConstant.PROVINCE_JSON, ProvinceDTO.class);
+        RegionDTO regionDTO = jsonDataLoader.getFromCode(place.getRegion().getRegionCode(),AppConstant.REGION_JSON, RegionDTO.class);
 
         return new PlaceDTO(place.getId(), place.getBuildingName(),
         barangayDTO,municipalityDTO,provinceDTO,regionDTO);
     }
-
-    public void  mapToModel(PlaceDTO placeDTO, Place place){
-
-        if(place.getDefaultPlace()!=null){
-            place.setDefaultPlace(placeDTO.getDefaultPlace());
-            return;
-        }
-
-        Barangay barangay=this.toBarangay(placeDTO.getBarangay());
-        Municipality municipality=this.toMunicipality(placeDTO.getMunicipality());
-        Province province=this.toProvince(placeDTO.getProvince());
-
-        place.setBuildingName(placeDTO.getBuildingName());
-        place.setBarangay(barangay);
-        place.setMunicipality(municipality);
-        place.setProvince(province);
-
-    }
-
-
 
 }
