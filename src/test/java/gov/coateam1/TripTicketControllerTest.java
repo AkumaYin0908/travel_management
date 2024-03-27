91e29095d246b95b5d584417f39628d8d8f6eeae
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-27 20:08:37 +0800

    write necessary annotations and declare necessary properties

--- src/test/java/gov/coateam1/TripTicketControllerTest.java
+++ src/test/java/gov/coateam1/TripTicketControllerTest.java
@@ -1,4 +1,31 @@
 package gov.coateam1;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.coateam1.model.trip_ticket.TripTicket;
+import gov.coateam1.service.TripTicketService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(TripTicket.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+@TestPropertySource(locations = "classpath:application.yml")
 public class TripTicketControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private TripTicketService tripTicketService;
+
+
 }
