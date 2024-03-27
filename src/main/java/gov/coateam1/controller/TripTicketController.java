cbc7e7dd916ae76978ebb2c2e95ab4ede5cc952f
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-27 20:06:12 +0800

    create getMapping with endpoint "/all"

--- src/main/java/gov/coateam1/controller/TripTicketController.java
+++ src/main/java/gov/coateam1/controller/TripTicketController.java
@@ -1,10 +1,16 @@
 package gov.coateam1.controller;
 
 
+import gov.coateam1.payload.trip_ticket.TripTicketDTO;
 import gov.coateam1.service.TripTicketService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,6 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TripTicketController {
 
     private  final TripTicketService tripTicketService;
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<TripTicketDTO>> getAllTripTicket(){
+        return new ResponseEntity<>(tripTicketService.findAll(), HttpStatus.OK);
+    }
 
 
 }
