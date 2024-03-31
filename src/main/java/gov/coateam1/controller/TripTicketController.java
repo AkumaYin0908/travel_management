911c8ce80ab9856353acc99697b2f1ca9cf55e94
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-03-31 13:40:19 +0800

    write remaining crud operation method

--- src/main/java/gov/coateam1/controller/TripTicketController.java
+++ src/main/java/gov/coateam1/controller/TripTicketController.java
@@ -1,20 +1,18 @@
 package gov.coateam1.controller;
 
 
+import gov.coateam1.payload.APIResponse;
 import gov.coateam1.payload.trip_ticket.TripTicketDTO;
 import gov.coateam1.service.TripTicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/trip-ticket")
 public class TripTicketController {
 
     private  final TripTicketService tripTicketService;
@@ -24,6 +22,29 @@ public class TripTicketController {
     public ResponseEntity<List<TripTicketDTO>> getAllTripTicket(){
         return new ResponseEntity<>(tripTicketService.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping("/triptickets/{id}")
+    public ResponseEntity<TripTicketDTO> getTripTicketById(@PathVariable("id")Long id){
+        return new ResponseEntity<>(tripTicketService.findById(id),HttpStatus.FOUND);
+    }
+
+
+    @PostMapping("/employees/{id}/triptickets")
+    public ResponseEntity<TripTicketDTO> saveTripTicket(@PathVariable("id")Long id, @RequestBody TripTicketDTO tripTicketDTO) throws Exception{
+        return new ResponseEntity<>(tripTicketService.add(id, tripTicketDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/triptickets/{id}")
+    public ResponseEntity<TripTicketDTO> updateTripTicket(@RequestBody TripTicketDTO tripTicketDTO, @PathVariable("id") Long id) throws Exception{
+        return new ResponseEntity<>(tripTicketService.update(tripTicketDTO,id),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/tripticket/{id}")
+    public ResponseEntity<APIResponse> deleteTripTicket(@PathVariable("id")Long id){
+        tripTicketService.delete(id);
+        return new ResponseEntity<>(new APIResponse("Delete successful!",true,HttpStatus.OK.value()),HttpStatus.OK);
+    }
+
 
 
 }
