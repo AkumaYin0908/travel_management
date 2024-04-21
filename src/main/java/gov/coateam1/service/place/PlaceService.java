15b40954935ebc881e1bfee17465e507eef4485f
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-04-22 05:42:54 +0800

    add findPlaceByCodes, remove comments and add throws Exception to method signatures

--- src/main/java/gov/coateam1/service/place/PlaceService.java
+++ src/main/java/gov/coateam1/service/place/PlaceService.java
@@ -1,29 +1,23 @@
 package gov.coateam1.service.place;
 
+import gov.coateam1.model.place.Place;
 import gov.coateam1.payload.place.PlaceDTO;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PlaceService {
 
-    PlaceDTO findById(Long id);
+    PlaceDTO findById(Long id) throws Exception;
 
-//    PlaceDTO findByBarangayName(String barangayName);
+    PlaceDTO findPlaceByCodes(PlaceDTO placeDTO) throws Exception;
 
-//    PlaceDTO findByMunicipalityName(String municipalityName);
-//
-//    PlaceDTO findByProvinceName(String provinceName);
-//
-//    PlaceDTO findByBuildingName(String buildingName);
+    PlaceDTO findByDefaultPlace(String defaultPlace) throws Exception;
 
-    PlaceDTO findByDefaultPlace(String defaultPlace);
-
-//    PlaceDTO findByCompletePlaceDetails(String buildingName, String barangay,
-//                                                                  String municipality, String province);
 
     List<PlaceDTO> findAll();
 
-    PlaceDTO add(PlaceDTO placeDTO);
+    PlaceDTO add(PlaceDTO placeDTO) throws Exception;
 
     PlaceDTO update(PlaceDTO placeDTO, Long id);
 
