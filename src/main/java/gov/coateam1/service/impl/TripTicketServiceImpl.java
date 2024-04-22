c6cc97c283daf090be0063f93db64c7f8f12bb17
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-04-23 03:13:10 +0800

    remove getPlaceFromDTO, getPlaceFromRepository and constructPlace methods and refactor method implementation of getConvertedPlaces method

--- src/main/java/gov/coateam1/service/impl/TripTicketServiceImpl.java
+++ src/main/java/gov/coateam1/service/impl/TripTicketServiceImpl.java
@@ -2,6 +2,7 @@ package gov.coateam1.service.impl;
 
 import gov.coateam1.constants.AppConstant;
 import gov.coateam1.exception.ResourceNotFoundException;
+import gov.coateam1.mapper.PlaceMapper;
 import gov.coateam1.model.Purpose;
 import gov.coateam1.model.Vehicle;
 import gov.coateam1.model.employee.Driver;
@@ -25,8 +26,10 @@ import gov.coateam1.repository.VehicleRepository;
 import gov.coateam1.repository.employee.EmployeeRepository;
 import gov.coateam1.repository.place.*;
 import gov.coateam1.service.TripTicketService;
+import gov.coateam1.service.place.PlaceService;
 import gov.coateam1.util.DateTimeConverter;
 import gov.coateam1.util.JSONDataLoader;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
@@ -47,13 +50,9 @@ public class TripTicketServiceImpl implements TripTicketService {
     private final TripTicketRepository tripTicketRepository;
     private final EmployeeRepository employeeRepository;
     private final VehicleRepository vehicleRepository;
-    private final PlaceRepository placeRepository;
-    private final JSONDataLoader jsonDataLoader;
-    private final BarangayRepository barangayRepository;
-    private  final MunicipalityRepository municipalityRepository;
-    private final ProvinceRepository provinceRepository;
-    private final RegionRepository regionRepository;
+    private final PlaceMapper placeMapper;
     private final ModelMapper modelMapper;
+    private final PlaceService placeService;
     private final PurposeRepository purposeRepository;
 
 
@@ -155,71 +154,18 @@ public class TripTicketServiceImpl implements TripTicketService {
         tripTicketRepository.deleteById(id);
     }
 
+    @Transactional
     private Set<Place> getConvertedPlaces(Set<PlaceDTO> placeDTOs) throws Exception {
         Set<Place> places = new LinkedHashSet<>();
         for (PlaceDTO placeDTO : placeDTOs) {
-            Place place = getPlaceFromDTO(placeDTO);
+            Place place = placeMapper.mapToModel(placeService.findPlaceByCodes(placeDTO));
             places.add(place);
-        }
 
+        }
         return places;
     }
 
-    public Place getPlaceFromDTO(PlaceDTO placeDTO) throws Exception {
-        Optional<Place> placeOptional = placeRepository.findById(placeDTO.getId());
-        if (placeOptional.isPresent()) {
-            return placeOptional.get();
-        } else {
-            if (placeDTO.getDefaultPlace() == null || placeDTO.getDefaultPlace().equals("N/A")) {
-                return getPlaceFromRepository(placeDTO);
-            } else {
-                return placeRepository.findByDefaultPlace(placeDTO.getDefaultPlace()).orElse(new Place(placeDTO.getDefaultPlace()));
-            }
-        }
-    }
 
-    public Place getPlaceFromRepository(PlaceDTO placeDTO) throws Exception {
-        Optional<Place> optionalPlace = placeRepository.findPlaceByCodes(
-                placeDTO.getBuildingName(),
-                placeDTO.getBarangay() == null ? null : placeDTO.getBarangay().getCode(),
-                placeDTO.getMunicipality().getCode(),
-                placeDTO.getProvince().getCode(),
-                placeDTO.getRegion().getCode()
-        );
-
-        return  optionalPlace.orElse(constructPlace(placeDTO));
-
-    }
-
-    private Place constructPlace(PlaceDTO placeDTO) throws Exception {
-        Place place = new Place();
-        place.setBuildingName(placeDTO.getBuildingName());
-        if (placeDTO.getBarangay() == null) {
-            place.setBarangay(null);
-        } else {
-            BarangayDTO barangayDTO = jsonDataLoader.getFromCode(placeDTO.getBarangay().getCode(), AppConstant.BARANGAY_JSON, BarangayDTO.class);
-            Barangay barangay = new Barangay(barangayDTO.getCode(), barangayDTO.getName());
-            barangay.addPlace(place);
-            barangayRepository.save(barangay);
-
-        }
-        MunicipalityDTO municipalityDTO = jsonDataLoader.getFromCode(placeDTO.getMunicipality().getCode(), AppConstant.MUNICIPALITY_JSON, MunicipalityDTO.class);
-        Municipality municipality = modelMapper.map(municipalityDTO, Municipality.class);
-        municipality.addPlace(place);
-        municipalityRepository.save(municipality);
-
-        ProvinceDTO provinceDTO = jsonDataLoader.getFromCode(placeDTO.getProvince().getCode(), AppConstant.PROVINCE_JSON, ProvinceDTO.class);
-        Province province = new Province(provinceDTO.getCode(), provinceDTO.getName());
-        province.addPlace(place);
-        provinceRepository.save(province);
-
-        RegionDTO regionDTO = jsonDataLoader.getFromCode(placeDTO.getRegion().getCode(), AppConstant.REGION_JSON, RegionDTO.class);
-        Region region = new Region(regionDTO.getCode(), regionDTO.getName());
-        region.addPlace(place);
-        regionRepository.save(region);
-
-        return place;
-    }
 
     private Set<Passenger> getConvertedPassengers(Set<EmployeeDTO> employeeDTOs){
         Set<Passenger> passengers = new LinkedHashSet<>();
