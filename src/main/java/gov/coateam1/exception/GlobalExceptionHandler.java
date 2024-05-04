8273ea5aca5c2ad839056ec50cfff841347915c5
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-04 20:32:45 +0800

    add exception handler method to handle exception captured when user inputs incorrect data based on the valid annotations

--- src/main/java/gov/coateam1/exception/GlobalExceptionHandler.java
+++ src/main/java/gov/coateam1/exception/GlobalExceptionHandler.java
@@ -1,10 +1,17 @@
 package gov.coateam1.exception;
 
+import ch.qos.logback.core.model.processor.ModelHandlerException;
 import gov.coateam1.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -34,6 +41,20 @@ public class GlobalExceptionHandler {
         APIResponse apiResponse = new APIResponse(message,false,HttpStatus.INTERNAL_SERVER_ERROR.value());
 
         return  new ResponseEntity<>(apiResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        Map<String, String> response = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach(
+                (objectError -> {
+                    String fieldName = ((FieldError)objectError).getField();
+                    String message = objectError.getDefaultMessage();
+                    response.put(fieldName, message);
+                })
+        );
+
+        return  new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
     }
 
 
